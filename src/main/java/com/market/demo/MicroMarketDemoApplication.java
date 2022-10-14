7618b5b41333f5a2f55136a4034c4d0596493192
juaneins_uio <ballesterosjg@gmail.com>
2022-10-13 21:42:24 -0500

    Initial commit

---
+++ src/main/java/com/market/demo/MicroMarketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.market.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMarketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroMarketDemoApplication.class, args);
+	}
+
+}
