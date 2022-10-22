20d1a98caddc512177662c3d3ebed336f5ff8e87
juaneins_uio <ballesterosjg@gmail.com>
2022-10-22 17:44:55 -0500

    Product repository

--- src/main/java/com/market/demo/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/market/demo/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,10 @@
  */
 package com.market.demo.persistence.crud;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.market.demo.persistence.entity.Producto;
@@ -13,4 +17,14 @@ import com.market.demo.persistence.entity.Producto;
  */
 public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
 
+	List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+	@Query(name = "select * from productos where id_categoria = ?", nativeQuery = true)
+	List<Producto> getByCategoria(int idCategoria);
+
+	Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+	Optional<Producto> findByCodigoBarras(String codigoBarras);
+
+	//Optional<Producto> findById(Long id);
 }
--- src/main/java/com/market/demo/persistence/crud/ProductoRepository.java
+++ src/main/java/com/market/demo/persistence/crud/ProductoRepository.java
@@ -4,6 +4,9 @@
 package com.market.demo.persistence.crud;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
 
 import com.market.demo.persistence.entity.Producto;
 
@@ -11,11 +14,36 @@ import com.market.demo.persistence.entity.Producto;
  * @author juaneins_uio
  *
  */
+@Repository
 public class ProductoRepository {
 	private ProductoCrudRepository productoCrudRepository;
-	
+
 	public List<Producto> getAll() {
 		return (List<Producto>) productoCrudRepository.findAll();
 	}
+
+	public List<Producto> getByCategoria(int idCategoria) {
+		return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+	}
+
+	public Optional<List<Producto>> getEscasos(int cantidad) {
+		return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+	}
 	
+	public Optional<Producto> getByCodigoBarras(String codigoBarras) {
+		return productoCrudRepository.findByCodigoBarras(codigoBarras);
+	}
+	
+	public Optional<Producto> getProducto(Long id) {
+		return productoCrudRepository.findById(id);
+	}
+	
+	public Producto save(Producto p) {
+		return productoCrudRepository.save(p);
+	}
+	
+	public void delete(Long id) {
+		productoCrudRepository.deleteById(id);
+	}
+
 }
