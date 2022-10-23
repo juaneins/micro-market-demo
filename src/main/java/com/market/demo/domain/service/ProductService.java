a78a8fec8eed5b3c0e4deb0e9f3c3a5a5d39eacd
juaneins_uio <ballesterosjg@gmail.com>
2022-10-23 08:07:50 -0500

    implement product service

---
+++ src/main/java/com/market/demo/domain/service/ProductService.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.market.demo.domain.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.market.demo.domain.Product;
+import com.market.demo.domain.repository.ProductRepository;
+
+/**
+ * @author juaneins_uio
+ *
+ */
+@Service
+public class ProductService {
+	
+	private ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {		
+		this.productRepository = productRepository;
+	}
+	
+	public List<Product> getAll() {
+		return productRepository.getAll();
+	}
+	
+	public Optional<Product> getProduct(int productId) {
+		return productRepository.getProduct(productId);
+	}
+	
+	Optional<List<Product>> getByCategory(int categoryId) {
+		return productRepository.getByCategory(categoryId);
+	}	
+
+	public  Product save(Product product) {
+		return productRepository.save(product);
+	}
+
+	public boolean delete(int productId) {
+		return getProduct(productId).map(product -> {
+			productRepository.save(product);
+			return true;
+		}).orElse(false);
+	}
+
+}
--- src/main/java/com/market/demo/persistence/crud/ProductoRepository.java
+++ src/main/java/com/market/demo/persistence/crud/ProductoRepository.java
@@ -19,24 +19,25 @@ import com.market.demo.persistence.mapper.ProductMapper;
  */
 @Repository
 public class ProductoRepository implements ProductRepository {
+
 	private ProductoCrudRepository productoCrudRepository;
 	private ProductMapper mapper;
+
+	public ProductoRepository(ProductoCrudRepository productoCrudRepository, ProductMapper mapper) {
+		this.productoCrudRepository = productoCrudRepository;
+		this.mapper = mapper;
+	}
 
 	@Override
 	public List<Product> getAll() {
 		List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
 		return mapper.toProducts(productos);
 	}
-	
+
 	public Optional<Producto> getByCodigoBarras(String codigoBarras) {
 		return productoCrudRepository.findByCodigoBarras(codigoBarras);
 	}
-	
-	public Producto save(Producto producto) {
-		return productoCrudRepository.save(producto);
-	}
-	
-	
+
 	@Override
 	public Optional<List<Product>> getByCategory(int categoryId) {
 		List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
@@ -45,22 +46,22 @@ public class ProductoRepository implements ProductRepository {
 
 	@Override
 	public Optional<List<Product>> getScarseProducts(int quantity) {
-		Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+		Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity,
+				true);
 		return productos.map(prod -> mapper.toProducts(prod));
 	}
 
 	@Override
 	public Optional<Product> getProduct(int productId) {
 		return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
-
 	}
 
 	@Override
 	public Product save(Product product) {
 		Producto producto = mapper.toProducto(product);
-		return mapper.toProduct(productoCrudRepository.save(producto));		
+		return mapper.toProduct(productoCrudRepository.save(producto));
 	}
-	
+
 	@Override
 	public void delete(int id) {
 		productoCrudRepository.deleteById(id);
