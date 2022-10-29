8469d3b61d5a68cf6eb996530978cf4a647a7953
juaneins_uio <ballesterosjg@gmail.com>
2022-10-29 18:30:32 -0500

    jwt authorization

--- src/main/java/com/market/demo/web/security/SecurityConfig.java
+++ src/main/java/com/market/demo/web/security/SecurityConfig.java
@@ -8,14 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.market.demo.domain.service.LocalAuthUserDetailsService;
+import com.market.demo.web.security.filter.JwtFilterRequest;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private LocalAuthUserDetailsService userDetailService;
+	
+	@Autowired
+	private JwtFilterRequest jwtFilterRequest;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -33,7 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
 		.antMatchers("/**/authenticate").permitAll()
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Override
